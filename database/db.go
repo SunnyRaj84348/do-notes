246474fa2fc6c7d08a6166c32d9068dcf29fb667
SunnyRaj84348 <sunnyraj7461@gmail.com>
2023-02-17 18:17:21 +0530

    db: Init connection
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- database/db.go
+++ database/db.go
@@ -1,1 +1,17 @@
 package database
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect(conn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", conn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	return db, err
+}
--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/SunnyRaj84348/do-notes/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	err := router.Run(":8080")
+	db, err := database.Connect(os.Getenv("MYSQL_STR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = router.Run(":8080")
 	if err != nil {
 		log.Fatalf("Could not start the http server: %v", err)
 	}

576ea38560b7a09c86a8df24fc577a95d6f505d6
SunnyRaj84348 <sunnyraj7461@gmail.com>
2023-02-17 18:25:10 +0530

    api: Create signup endpoint
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/SunnyRaj84348/do-notes/database"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type Credential struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 func main() {
 	router := gin.Default()
@@ -15,6 +22,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	router.POST("/signup", func(ctx *gin.Context) {
+		cred := Credential{}
+
+		err := ctx.BindJSON(&cred)
+		if err != nil {
+			return
+		}
+
+		hashPass, err := bcrypt.GenerateFromPassword([]byte(cred.Password), bcrypt.DefaultCost)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+		}
+
+		err = database.InsertUser(db, cred.Username, string(hashPass))
+		if err != nil {
+			ctx.JSON(http.StatusConflict, gin.H{
+				"message": "user already exist",
+			})
+
+			return
+		}
+	})
 
 	err = router.Run(":8080")
 	if err != nil {

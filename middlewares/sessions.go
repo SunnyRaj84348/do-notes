1ef51e65d5c6d5b58657270d86aa173f2dcb1a81
SunnyRaj84348 <sunnyraj7461@gmail.com>
2023-03-10 14:17:09 +0530

    cookie: Set secure option
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- middlewares/sessions.go
+++ middlewares/sessions.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/SunnyRaj84348/do-notes/utilities"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
@@ -11,8 +13,10 @@ func Sessions() gin.HandlerFunc {
 	// Create new cookie store with secure auth
 	store := cookie.NewStore(utilities.RandToken())
 	store.Options(sessions.Options{
-		Secure: false,
-		MaxAge: 60 * 60 * 24 * 30,
+		MaxAge:   60 * 60 * 24 * 30,
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
 	})
 
 	return sessions.Sessions("session_user", store)
